f906959dcd300ea1a5baa44ebad498870ab43dd2
yu-yk <yu_yukkuen@retail-ai.jp>
2022-05-23 20:30:39 +0900

    make windows support

--- bc.go
+++ bc.go
@@ -7,12 +7,8 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"os/exec"
-	"path/filepath"
 	"strconv"
-	"strings"
 	"sync"
-	"syscall"
 	"time"
 
 	"github.com/go-resty/resty/v2"
@@ -132,51 +128,6 @@ LOOP:
 	c.CurrStatus.Write([]byte(completed + "\n"))
 	c.ErrorLogFile.Close()
 	c.FfmpegErrLogFile.Close()
-}
-
-// Download the video from the response content by spwaning a `ffmpeg` child process.
-func saveVideo(respMap map[string]interface{}, path string, logFile io.Writer) error {
-	ss := respMap["sources"].([]interface{})
-	s := ss[0].(map[string]interface{})
-	url := s["src"].(string)
-	vid, name := respMap["id"].(string), respMap["name"].(string)
-	name = strings.ReplaceAll(name, "/", "-")
-	name = vid + "_" + name + ".mp4"
-	filePath := filepath.Join(path, name)
-
-	ffmpegCmd := exec.Command("ffmpeg", "-i", url, "-c", "copy", filePath, "-loglevel", "error")
-	ffmpegCmd.SysProcAttr = &syscall.SysProcAttr{
-		Setpgid: true,
-	}
-	ffmpegCmd.Stderr = logFile
-	if err := ffmpegCmd.Run(); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// Save the response as `$vid_$name.json` format.
-func saveResp(respMap map[string]interface{}, path string) error {
-	prettyJsonMsg, err := prettilize(respMap)
-	if err != nil {
-		return err
-	}
-
-	vid, name := respMap["id"].(string), respMap["name"].(string)
-	name = strings.ReplaceAll(name, "/", "-")
-	name = vid + "_" + name + ".json"
-	filePath := filepath.Join(path, name)
-	file, err := os.Create(filePath)
-	if err != nil {
-		return err
-	}
-
-	if _, err := file.Write(prettyJsonMsg); err != nil {
-		return err
-	}
-
-	return nil
 }
 
 // Add indentation to the json string.
